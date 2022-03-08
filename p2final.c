4e182b4935a0711eb48dfc733d3641335059d6a5
Samson <95264901+54MMYYY@users.noreply.github.com>
2022-03-08 04:01:27 +0000

    .

--- p2final.c
+++ p2final.c
@@ -1,1 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+struct _fraction
+{
+int num,den;
+}
+typedef _fraction Fraction(){
+  
+}
+Fraction input_fraction(){
+  
+}
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3){
+  
+}
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest){
+  
+}
+
